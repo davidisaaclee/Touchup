74eb59efc8fb6861edcff5a7d74b62460ea21fe3
David Lee <aewofij@gmail.com>
2017-03-09 14:06:02 -0500

    Smooth out eraser

--- CITransformsExperiment/EraserTool.swift
+++ CITransformsExperiment/EraserTool.swift
@@ -17,20 +17,24 @@ class EraserTool {
 
 	weak var delegate: EraserToolDelegate?
 
+	fileprivate struct Spline {
+		var point: CGPoint
+		var controlPoint1: CGPoint
+		var controlPoint2: CGPoint
+
+		static func straight(on point: CGPoint) -> Spline {
+			return Spline(point: point, controlPoint1: point, controlPoint2: point)
+		}
+	}
+
 	fileprivate enum Mode {
 		case passive
-		case active([CGPoint])
+		case active([Spline])
 	}
 
 	fileprivate var mode: Mode = .passive
 
-//	var workingPath: CGMutablePath? {
-//		guard case let .active(path) = mode else {
-//			return nil
-//		}
-//
-//		return path
-//	}
+	private let smoothFactor: CGFloat = 0.2
 
 	func begin(with touch: UITouch) {
 		guard let delegate = delegate else {
@@ -38,7 +42,7 @@ class EraserTool {
 		}
 
 		let location = delegate.eraserTool(self, locationFor: touch)
-		mode = .active([location])
+		mode = .active([.straight(on: location)])
 		delegate.eraserTool(self, didBeginDrawingAt: location)
 	}
 
@@ -50,20 +54,19 @@ class EraserTool {
 				return
 			}
 
-		// Not actually useful :(
-//		if let b = points.last, let a = points.dropLast().last {
-//			let c = delegate.eraserTool(self, locationFor: touch)
-//			// calling the last three points (a, b, c), where c is the point about to be added
-//
-//			let ab = b - a
-//			let ac = c - a
-//			if ab.rejection(on: ac).magnitude < (ac.magnitude / 4) {
-////				print("Simplifying...")
-//				points.removeLast()
-//			}
-//		}
+		var c: Spline = .straight(on: delegate.eraserTool(self, locationFor: touch))
 
-		points.append(delegate.eraserTool(self, locationFor: touch))
+		// if we have at least two points in the line, we can start to smooth.
+		if var b = points.last, let a = points.dropLast().last {
+			// calling the last three points (a, b, c), where c is the point about to be added
+			let ac = c.point - a.point
+			c.controlPoint1 = smoothFactor * ac + b.point
+			b.controlPoint2 = -smoothFactor * ac + b.point
+			points.removeLast()
+			points.append(b)
+		}
+		points.append(c)
+
 		mode = .active(points)
 
 		delegate.eraserTool(self, didUpdateWorkingPath: makePath(for: points))
@@ -81,9 +84,14 @@ class EraserTool {
 		mode = .passive
 	}
 
-	private func makePath(for points: [CGPoint]) -> CGPath {
-		return UIBezierPath(points: points,
-		                    smoothFactor: 0).cgPath
+	private func makePath(for points: [Spline]) -> CGPath {
+		let path = CGMutablePath()
+		points.first.map { path.move(to: $0.point) }
+		points.dropFirst()
+			.forEach { path.addCurve(to: $0.point,
+			                         control1: $0.controlPoint1,
+			                         control2: $0.controlPoint2) }
+		return path
 	}
 
 }
